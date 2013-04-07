a2af3e39e36bb6c5904c958c9db9098d1564de89
Archon <archon@archons.info>
2013-04-07 23:42:57 +0200

    Example of object - Hero

--- Objects/objects.js
+++ Objects/objects.js
@@ -5,3 +5,14 @@ var obj = {};
 
 // Not recomended object deffinition
 var obj2 = new Object();
+
+// Example of object
+var here = {
+	name: 'Superman',
+	hands: 2,
+	lengs: 2,
+	superPower: 100
+	fly() {
+		//here definition of flying...
+	}
+}
