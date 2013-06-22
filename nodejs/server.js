43329baadf600aefd5e49b369027ef207cedf919
Archon <archon@archons.info>
2013-06-22 18:59:25 +0200

    Respond with HTML

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,8 +1,7 @@
 var http = require('http');
-
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-
+var server = http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/html'});
+res.end('<html><head><title>Foobar</title></head><body><h1>Hello world!</h1></body></html>');
+});
+server.listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
