8a28aa0a2d811a842ca6aac4b4c129038d648620
Archon <archon@archon.info>
2014-11-22 19:09:49 +0100

    console.log to emit

--- socket.io/chat/index.js
+++ socket.io/chat/index.js
@@ -8,6 +8,7 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
+    console.log('Emiting msg: ' + msg);
     io.emit('chat message', msg);
   });
 });
